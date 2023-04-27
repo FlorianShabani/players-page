c3ceddeb14e1e2907b59c7c05f05237d39133c6d
Florian <florian@91.life>
2023-04-27 15:14:46 +0200

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+  
 export default function Home() {
-    return(
-        <>
-            <h1>Players Page</h1>
-        </>
-    )
+return (
+  <>
+    <h1>Players API</h1>
+
+    <Link href="/players">See Players</Link>
+  </>
+)
 }
