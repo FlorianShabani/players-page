9f14bff911eb72370b4d4e1f359d120e82830069
Florian <florian@91.life>
2023-05-02 12:01:00 +0200

    Card fix and form css and html

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -13,41 +13,41 @@ export default function Player({ player, handleDelete }) {
     } else if (cardType === "Delete") {
         Card = <DeletePlayer player={player} handleDelete={handleDelete} />
     } else if (cardType === "Edit") {
-        Card = <EditPlayer player={player}/>
+        Card = <EditPlayer player={player} />
     }
 
 
     return (
         <>
             <div className={styles.card}>
-                <PlayerTools switchDelete={() => setCardType("Delete")} switchEdit={() => setCardType("Edit")}/>
-                {cardType === "View" && <Link href={`/player/${player.id}`}/>}
-                    {Card}
+                <PlayerTools cardType={cardType} switchDelete={() => setCardType("Delete")} switchEdit={() => setCardType("Edit")} switchView={() => setCardType("View")
+                } />
+                {cardType === "View" && <Link href={`/player/${player.id}`} />}
+                {Card}
             </div >
         </>
     )
 }
 
-function PlayerTools({ switchView, switchDelete, switchEdit }) {
+function PlayerTools({ cardType, switchView, switchDelete, switchEdit }) {
     return (
         <div className={styles.tools}>
-            <div className={styles.circle} onClick={switchDelete}>
-                <span className={`${styles.red} ${styles.box}`}>
-
-                </span>
-            </div>
-            <div className={styles.circle} onClick={switchEdit}>
-                <span className={`${styles.yellow} ${styles.box}`}>
-
-                </span>
-            </div>
+            {cardType !== "Delete" &&
+                <div className={`${styles.circle} ${styles.red} ${styles.box}`} onClick={switchDelete}>
+                </div>}
+            {cardType !== "View" &&
+                <div className={`${styles.circle} ${styles.green} ${styles.box}`} onClick={switchView}>
+                </div>}
+            {cardType !== "Edit" &&
+                <div className={`${styles.circle} ${styles.yellow} ${styles.box}`} onClick={switchEdit}>
+                </div>}
         </div>
     )
 }
 
 function ViewPlayer({ player }) {
     return (
-        <div className={styles.card_content}>
+        <div className={styles.cardContent}>
             <p className={styles.text}>{player.id}</p>
             <p className={styles.text}>{player.name}</p>
             <p className={styles.text}>{player.age}</p>
@@ -59,12 +59,13 @@ function ViewPlayer({ player }) {
 function DeletePlayer({ player, handleDelete }) {
     return (
         <div className={styles.cardContent}>
-            <button className={styles.deleteButton} onClick={handleDelete}>Delete</button>
+            <label className={styles.deleteText} for="deleteButton">Delete Player?</label>
+            <button id="deleteButton" className={styles.deleteButton} onClick={handleDelete}>Delete</button>
         </div>
     )
 }
 
-function EditPlayer ({ player, handleEdit}) {
+function EditPlayer({ player, handleEdit }) {
     return (
         <div className={styles.cardContent}>
             <button className={styles.editButton} onClick={handleEdit}>Edit</button>
--- pages/createPlayer.js
+++ pages/createPlayer.js
@@ -1,3 +1,40 @@
-export default functiona createPlayer() {
-    
+import styles from '../styles/createPlayer.module.css'
+
+export default function createPlayer() {
+    return (
+        <div className={styles.body}>
+            <div className={styles.form}>
+                <>
+                    <div className={styles.title}>Welcome</div>
+                    <div className={styles.subtitle}>Let's create your account!</div>
+                    <div className={`${styles.inputContainer} ${styles.ic1}`}>
+                        <input id="name" className={styles.input} type="text" placeholder=" " />
+                        <div className={styles.cut} />
+                        <label htmlFor="name" className={styles.placeholder}>
+                            First name
+                        </label>
+                    </div>
+                    <div className={`${styles.inputContainer} ${styles.ic2}`}>
+                        <input id="team" className={styles.input} type="text" placeholder=" " />
+                        <div className={styles.cut} />
+                        <label htmlFor="team" className={styles.placeholder}>
+                            Last name
+                        </label>
+                    </div>
+
+                    <div className={`${styles.inputContainer} ${styles.ic2}`}>
+                        <input id="age" className={styles.input} type="number" placeholder=" " />
+                        <div className={styles.cut} />
+                        <label htmlFor="age" className={styles.placeholder}>
+                            Last name
+                        </label>
+                    </div>
+                    
+                    <button type="text" className={styles.submit}>
+                        submit
+                    </button>
+                </>
+            </div>
+        </div>
+    )
 }
